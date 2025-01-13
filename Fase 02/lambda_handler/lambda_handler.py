fb7465e1f44756ae63686e08c7b8d981f0f8da3a
Felipe F. Porto <106607666+felipefporto@users.noreply.github.com>
2025-01-12 21:44:00 -0300

    Update lambda_handler.py
    Add docstring

--- Fase 02/lambda_handler/lambda_handler.py
+++ Fase 02/lambda_handler/lambda_handler.py
@@ -1,6 +1,48 @@
 import boto3
 
 def lambda_handler(event, context):
+    """
+    Função Lambda para verificar e iniciar um job do AWS Glue.
+
+    Esta função verifica a existência de um job do AWS Glue com o nome especificado 
+    e, se encontrado, inicia a execução do job. Caso o job não seja encontrado ou ocorra 
+    algum erro, a função retorna mensagens apropriadas.
+
+    Parâmetros:
+        event (dict): Dados do evento que acionam a função Lambda (não utilizado neste caso).
+        context (object): Contexto de execução da função Lambda (contém informações como ID de execução).
+
+    Retorno:
+        dict: Um dicionário contendo:
+            - statusCode (int): Código de status HTTP indicando o resultado da operação.
+            - body (str): Mensagem explicativa sobre o resultado da operação.
+
+    Respostas possíveis:
+        - statusCode: 200
+          body: "Job iniciado com sucesso. JobRunId: <JobRunId>"
+          Indica que o job foi encontrado e iniciado com sucesso.
+
+        - statusCode: 404
+          body: "Job '<job_name>' não encontrado. Verifique se o nome está correto."
+          Indica que o job especificado não existe.
+
+        - statusCode: 500
+          body: "Erro ao iniciar o job: <mensagem de erro>"
+          Indica que ocorreu um erro inesperado durante a execução.
+
+    Exceções tratadas:
+        - glue_client.exceptions.EntityNotFoundException:
+            Disparada quando o job especificado não é encontrado no Glue.
+        - Exception:
+            Captura erros genéricos que podem ocorrer durante a execução da função.
+
+    Dependências:
+        - boto3: Biblioteca necessária para interagir com os serviços da AWS.
+        - Configuração adequada de permissões IAM para permitir acesso ao AWS Glue.
+
+    Observação:
+        Certifique-se de que o nome do job no código corresponde exatamente ao nome do job configurado no AWS Glue.
+    """
     glue_job_name = "Glue TECH CHALLENGE 2 "
     glue_client = boto3.client('glue')
     
